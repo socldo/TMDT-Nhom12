6e7a78fd21b23f536c77d5abac74391393f47ff6
MinhQuan-21130491 <21130491@st.hcmuaf.edu.vn>
2024-05-10 10:05:52 +0700

    add cors

--- E-Commerce/src/index.js
+++ E-Commerce/src/index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { default: mongoose } = require("mongoose");
 const router = require("./routes");
+const cors = require('cors')
 const bodyParser = require("body-parser");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001
 
+app.use(cors())
 app.use(bodyParser.json());
 
 router(app);
-
-
 mongoose.connect(`mongodb+srv://lephilongd6ddt2021:${process.env.MONGO_DB}@cluster0.usoblmf.mongodb.net/`)
 .then(() => {
     console.log("connect success!");
